d810fd2a180328848385325f6d040d01d121d29d
Brian Abram <babram@udel.edu>
2017-11-07 13:39:01 -0500

    flash script added

--- songbase.py
+++ songbase.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>hello world!!!!</h1>'
+    return '<h1>hello world!!!! lalala</h1>'
 
 @app.route('/user/<string:name>/')
 def get_user(name):
     return '<h1>hello  %s your age is</h1>' % (name, 3)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
